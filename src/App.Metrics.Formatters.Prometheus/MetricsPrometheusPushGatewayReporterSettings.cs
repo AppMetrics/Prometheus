bbbef943c95e6473fee5849a0c0351d8f8e5d796
Brian Drupieski <brian.drupieski@gmail.com>
2019-03-10 09:46:25 -0400

    add prometheus push gateway reporter

---
+++ src/App.Metrics.Formatters.Prometheus/MetricsPrometheusPushGatewayReporterSettings.cs
@@ -0,0 +1,19 @@
+﻿// <copyright file="MetricsPrometheusPushGatewayReporterSettings.cs" company="App Metrics Contributors">
+// Copyright (c) App Metrics Contributors. All rights reserved.
+// </copyright>
+
+using System.Collections.Generic;
+
+namespace App.Metrics.Formatters.Prometheus
+{
+    public class MetricsPrometheusPushGatewayReporterSettings
+    {
+        public string Endpoint { get; set; }
+
+        public string Job { get; set; }
+
+        public string Instance { get; set; }
+
+        public IEnumerable<KeyValuePair<string, string>> AdditionalLabels { get; set; }
+    }
+}
